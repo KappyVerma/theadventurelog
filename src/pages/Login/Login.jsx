3972d8aa12ba2efe7fd30811b97d5af4861079b0
Karan Verma <karranverma@gmail.com>
2024-01-29 16:37:11 -0800

    finish working on login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,12 @@
 import "./Login.scss";
 
 export default function Login() {
-  return;
+  return (
+    <section className="login-page">
+      <div className="login-page__background"></div>
+      <p className="login-page__text">
+        Share you travel stories from far and wide
+      </p>
+    </section>
+  );
 }
