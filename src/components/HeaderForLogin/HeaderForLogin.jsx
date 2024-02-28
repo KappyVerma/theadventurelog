a34c814c62f564e70ecb0c11c96aa64e78524db2
Karan Verma <karranverma@gmail.com>
2024-02-28 15:34:13 -0800

    react

---
+++ src/components/HeaderForLogin/HeaderForLogin.jsx
@@ -0,0 +1,21 @@
+import "./HeaderForLogin.scss";
+import { Link } from "react-router-dom";
+
+export default function HeaderForLogin() {
+  return (
+    <header className="login-page-navBar">
+      <Link to="/" className="login-page-navBar__logo"></Link>
+      <nav className="login-page-navBar__list">
+        <Link to="/login" className="login-page-navBar__links">
+          Log in
+        </Link>
+        <Link
+          to="signup"
+          className="login-page-navBar__links login-page-navBar__links--mod"
+        >
+          Sign up
+        </Link>
+      </nav>
+    </header>
+  );
+}
